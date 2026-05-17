ad3cef2461b8efa2b5335ca6deb845c3813a9564
IT25103158 <it25103158@my.sliit.lk>
2026-05-17 07:33:52 +0530

    add test comment to Exam.java

--- src/main/java/com/exam/exam_system/model/Exam.java
+++ src/main/java/com/exam/exam_system/model/Exam.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
+//test commit
 @Entity
 @Table(name = "exams")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
